db8adc9c7b51ee9f7f782cc1fdc0315ae9b13dd5
Bruce Becker <brucellino@gmail.com>
2018-06-15 19:23:05 +0200

    Add failing wn metapackage test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -13,8 +13,11 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
-
-def test_umd_release(host):
-    p = host.package('umd-release')
-
+@pytest.mark.parametrize("name,version", [
+    ("epel-release","7.11"),
+    ("umd-release","4.1.3"),
+    ("wn","4.1.3")])
+def test_packages(host, name, version):
+    p = host.package(name)
     assert p.is_installed
+    assert p.version.startswith(version)
