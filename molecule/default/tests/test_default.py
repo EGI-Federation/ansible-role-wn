2b081a590476c9113bff14a78371df1372080201
Bruce Becker <brucellino@gmail.com>
2018-06-15 17:47:04 +0200

    Add requirements

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,8 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_umd_release(host):
+    p = host.package('umd-release')
+
+    assert p.is_installed
