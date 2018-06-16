f541f3cdb7e9bf594d5ccd74180f5476ee3bc4cd
Bruce Becker <brucellino@gmail.com>
2018-06-16 12:15:21 +0200

    Fix linting on python

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,22 +16,25 @@ def test_hosts_file(host):
 
 # Test CEntOS7 releases
 @pytest.mark.parametrize("name,version", [
-    ("epel-release","7.11"),
-    ("umd-release","4.1.3"),
-    ("wn","4.1.3")])
-def test_packages(host, name, version):
-    if host.system_info.distribution == 'redhat' and host.system_info.distribution.release.startswith(7):
+    ("epel-release", "7.11"),
+    ("umd-release", "4.1.3"),
+    ("wn", "4.1.3")])
+def test_release_centos7(host, name, version):
+    if (host.system_info.distribution == 'redhat' and
+            host.system_info.distribution.release.startswith(7)):
         p = host.package(name)
         assert p.is_installed
         assert p.version.startswith(version)
 
+
 # Test CEntOS6 releases
 @pytest.mark.parametrize("name,version", [
-    ("epel-release","6.8"),
-    ("umd-release","4.1.3"),
-    ("emi-wn","3.1.0")])
-def test_packages(host, name, version):
-    if host.system_info.distribution == 'redhat' and host.system_info.distribution.release.startswith(6):
+    ("epel-release", "6.8"),
+    ("umd-release", "4.1.3"),
+    ("emi-wn", "3.1.0")])
+def test_release_centos6(host, name, version):
+    if (host.system_info.distribution == 'redhat' and
+            host.system_info.distribution.release.startswith(6)):
         p = host.package(name)
         assert p.is_installed
         assert p.version.startswith(version)
--- molecule/default/tests/test_packages.py
+++ molecule/default/tests/test_packages.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 @pytest.mark.parametrize("name", [
     "c-ares",
     "cleanup-grid-accounts",
@@ -50,10 +51,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     "voms-clients-java",
     "voms-devel",
     "xrootd-client"])
-def test_dependencies_centos7(host,name):
-    if host.system_info.distribution == 'redhat' and host.system_info.distribution.release.startswith(7):
-      p=host.package(name)
-      assert p.is_installed
+def test_dependencies_centos7(host, name):
+    if (host.system_info.distribution == 'redhat' and
+            host.system_info.distribution.release.startswith(7)):
+        p = host.package(name)
+        assert p.is_installed
 
 
 @pytest.mark.parametrize("name", [
@@ -84,7 +86,8 @@ def test_dependencies_centos7(host,name):
   "lcg-util-libs",
   "lcg-util-python"
 ])
-def test_dependencies_centos6(host,name):
-  if host.system_info.distribution == 'redhat' and host.system_info.distribution.release.startswith(6):
-      p=host.package(name)
-      assert p.is_installed
+def test_dependencies_centos6(host, name):
+    if (host.system_info.distribution == 'redhat' and
+            host.system_info.distribution.release.startswith(6)):
+        p = host.package(name)
+        assert p.is_installed
